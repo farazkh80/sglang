87beb0eab06428ae47d4e5f5b64e4bcde009c9df
Faraz Khoubsirat <58580514+farazkh80@users.noreply.github.com>
2025-07-21 14:32:27 -0700

    neater interface
    Signed-off-by: Faraz Khoubsirat <58580514+farazkh80@users.noreply.github.com>

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -387,7 +387,7 @@ class ModelRunner:
                 if is_hopper_with_cuda_12_3():
                     server_args.attention_backend = "fa3"
                 elif is_sm100_supported():
-                    server_args.attention_backend = "trtllm_mla"
+                    server_args.attention_backend = "flashinfer"
                 elif _is_hip:
                     head_num = self.model_config.get_num_kv_heads(self.tp_size)
                     # TODO current aiter only support head number 16 or 128 head number
@@ -1326,11 +1326,11 @@ class ModelRunner:
 
             return CutlassMLABackend(self)
         elif self.server_args.attention_backend == "trtllm_mla":
-            from sglang.srt.layers.attention.trtllm_gen_mla_backend import (
-                TRTLLMGENMLABackend,
+            from python.sglang.srt.layers.attention.trtllm_mla_backend import (
+                TRTLLMMLABackend,
             )
 
-            return TRTLLMGENMLABackend(self)
+            return TRTLLMMLABackend(self)
         elif self.server_args.attention_backend == "intel_amx":
             from sglang.srt.layers.attention.intel_amx_backend import (
                 IntelAMXAttnBackend,
